273e3ecde235ad296f8ce20a873a1cb4be90f2c9
mohammed0789 <alibrahim0789@gmail.com>
2022-07-08 18:49:19 +0300

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -54,7 +54,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x21d709653d2177F1f3de2399A2BCC4dee8348d94';
 const TREASURY_ADDRESS = '0x21d709653d2177F1f3de2399A2BCC4dee8348d94';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 30; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 10; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
